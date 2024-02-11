34a592dfab9a910d42c277bde9752b3222837195
Valentin Krumov <valentinvpk@abv.bg>
2024-02-11 10:35:01 +0200

    Setting up Firebase Auth

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
   getDoc,
   updateDoc,
 } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHawIXgHTSn0-DbFlyRNE0XDLVuZY28k4",
@@ -28,6 +29,7 @@ initializeApp(firebaseConfig);
 
 //init services
 const db = getFirestore();
+const auth = getAuth();
 
 // collection ref
 const colRef = collection(db, "books");
